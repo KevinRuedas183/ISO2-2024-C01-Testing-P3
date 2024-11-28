5983ce85fdfbf31a8d0605478e2f126a243f2331
KevinRuedas183 <Kevin.Ruedas@alu.uclm.es>
2024-11-28 12:36:37 +0100

    Inicialización de proyecto para el problema03.

--- Ejercicio03/src/test/java/es/UCLM/esi/ISO2/C01/Ejercicio03/AppTest.java
+++ Ejercicio03/src/test/java/es/UCLM/esi/ISO2/C01/Ejercicio03/AppTest.java
@@ -1,8 +1,7 @@
 package es.UCLM.esi.ISO2.C01.Ejercicio03;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
