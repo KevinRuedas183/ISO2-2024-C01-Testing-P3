b1a56d0b03c8f4db55e447c1693f454308d447b2
Alejandro-Rodriguez-Santos <alejandro.rodriguez38@alu.uclm.es>
2024-12-18 17:05:49 +0100

    Correccion algunos test

--- Ejercicio03/src/test/java/es/UCLM/esi/ISO2/C01/Ejercicio03/RecomendadorActividadesTest.java
+++ Ejercicio03/src/test/java/es/UCLM/esi/ISO2/C01/Ejercicio03/RecomendadorActividadesTest.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 
-
 public class RecomendadorActividadesTest {
-	
 
 	private RecomendadorActividades recomendador; // Clase a testear
 	private Usuario usuario; // Usuario de prueba
@@ -17,20 +15,21 @@ public class RecomendadorActividadesTest {
 	@Before
 	public void setUp() throws Exception {
 		// Configuración inicial antes de cada prueba
-		
+
 		/*
-		 * usuario = new Usuario(00000000, 'A', false, false, false, false);
-		 * Esto es por si lo ponemos, si lo dejamos no hay que 
-		 * crear un objeto usuario  luego, solo usar sets para dar valores
-		 * Si no lo dejamos, hay que crear y poner valores a usuario.
+		 * Esto es por si lo ponemos, si lo dejamos no hay que crear un objeto usuario
+		 * luego, solo usar sets para dar valores Si no lo dejamos, hay que crear y
+		 * poner valores a usuario.
 		 * 
-		 * Para mi, dejo eso si los boolean, el objeto recomendador,
-		 * espacio y pronostico
+		 * Para mi, dejo eso si los boolean, el objeto recomendador, espacio y
+		 * pronostico
 		 */
+		// nota test --> DNI en usuario es un int, por lo que muchos numeros son mas grandes de lo que abarca
+		usuario = new Usuario(10504000, 'A', false, false, false, false);
+		pronostico = new PronosticoMeteorologico(0, 0, Precipitaciones.lluvia);
 		espacio = new EspacioDeOcio(0, 0, false, 0, 0, pronostico);
-		pronostico = new PronosticoMeteorologico(0, 0, null);
 		recomendador = new RecomendadorActividades();
-		
+
 		boolean climaNevado = false;
 		boolean climaEsquiable = false;
 		boolean climaSenderismo = false;
@@ -38,25 +37,25 @@ public class RecomendadorActividadesTest {
 		boolean climaCañas = false;
 		boolean climaPiscina = false;
 	}
-	
-	
 
- //Primer método  usuarioValido 
-    @Test
-    public void UsuarioValidoFtest() throws Exception {
-    	Usuario usuario=new Usuario (10504000, 'A', false, false, false, false);
-    	boolean actual=RecomendadorActividades.usuarioValido(usuario);
-    	boolean expected= false;
-    	assertEquals(actual, expected);
-    }
-    @Test
-    public void UsuarioValidoTtest() throws Exception {
-    	Usuario usuario=new Usuario (10504000, 'A', true, true, true, true);
-    	boolean actual=RecomendadorActividades.usuarioValido(usuario);
-    	boolean expected= true;
-    	assertEquals(actual, expected);
-    }
-    /*
+	// Primer método usuarioValido
+	@Test
+	public void UsuarioValidoFtest() throws Exception {
+		Usuario usuario = new Usuario(10504000, 'A', false, false, false, false);
+		boolean actual = RecomendadorActividades.usuarioValido(usuario);
+		boolean expected = false;
+		assertEquals(actual, expected);
+	}
+
+	@Test
+	public void UsuarioValidoTtest() throws Exception {
+		Usuario usuario = new Usuario(10504000, 'A', true, true, true, true);
+		boolean actual = RecomendadorActividades.usuarioValido(usuario);
+		boolean expected = true;
+		assertEquals(actual, expected);
+	}
+
+	/*
 	 * TEST MCDC
 	 */
 	/* test 2 (salida a true) */
@@ -110,14 +109,14 @@ public class RecomendadorActividadesTest {
 	}
 
 	/* test 7 (salida a false) */
-	@Test 
+	@Test
 	public void esClimaNevadoTRUE() throws Exception {
-	// Configuración del pronóstico
-		pronostico.setTemperatura(-10); 
+		// Configuración del pronóstico
+		pronostico.setTemperatura(-10);
 		pronostico.setPorcentajeHumedadRelativa(10);
 		pronostico.setPrecipitaciones(Precipitaciones.nubes); // nubes = no precipitaciones
 		espacio.setPronostico(pronostico);
-		
+
 		boolean actual = recomendador.esClimaNevado(pronostico);
 		boolean expected = true;
 		assertEquals(actual, expected);
@@ -126,89 +125,85 @@ public class RecomendadorActividadesTest {
 	/* test 8 (salida a true) */
 	@Test
 	public void esClimaEsquiableTRUE() throws Exception {
-	// Configuración del pronóstico
-		pronostico.setTemperatura(10); 
+		// Configuración del pronóstico
+		pronostico.setTemperatura(10);
 		pronostico.setPorcentajeHumedadRelativa(25);
 		pronostico.setPrecipitaciones(Precipitaciones.nubes); // nubes = no precipitaciones
 		espacio.setPronostico(pronostico);
-		
+
 		boolean actual = recomendador.esClimaEsquiable(pronostico);
 		boolean expected = true;
 		assertEquals(actual, expected);
 
-		
 	}
 
 	/* test 9 (salida a true) */
 	@Test
 	public void esClimaSenderismoTRUE() throws Exception {
-	// Configuración del pronóstico
-		pronostico.setTemperatura(-1); 
+		// Configuración del pronóstico
+		pronostico.setTemperatura(-1);
 		// no hace falta humedad relativa ya que lo tenemos en el before inicializado
 		pronostico.setPrecipitaciones(Precipitaciones.lluvia); // nubes = no precipitaciones
-		
+
 		espacio.setPronostico(pronostico);
-		
+
 		boolean actual = recomendador.esClimaSenderismo(pronostico);
 		boolean expected = true;
 		assertEquals(actual, expected);
-	
-		
+
 	}
 
-	/* test 10 (salida a true) 
+	/*
+	 * test 10 (salida a true)
 	 * 
 	 * AQUI FALTAN DATOS DE SUS TABLAS DE PRUEBA
 	 * 
-	 * */
+	 */
 	@Test
 	public void esClimaTurismoTRUE() throws Exception {
-	// Configuración del pronóstico
-		pronostico.setTemperatura(-10); 
+		// Configuración del pronóstico
+		pronostico.setTemperatura(-10);
 		pronostico.setPorcentajeHumedadRelativa(10);
 		pronostico.setPrecipitaciones(Precipitaciones.nieve); // nubes = no precipitaciones
 		espacio.setPronostico(pronostico);
-		
+
 		boolean actual = recomendador.esClimaTurismo(pronostico);
 		boolean expected = true;
 		assertEquals(actual, expected);
-	
-		
+
 	}
 
-	/* test 11 (salida a true)
+	/*
+	 * test 11 (salida a true)
 	 * 
-	 *  AQUI Y EN EL TEST DE esClimaPiscinaTRUE SE SUPONE QUE 
-	 *  PONIENDO NIEVE DA LO MISMO QUE PONIENDO 
-	 *  NUBES, ME PARECE RARO PERO ES LO QUE PONE EN LAS TABLAS
-	 *  
-	 *  */
+	 * AQUI Y EN EL TEST DE esClimaPiscinaTRUE SE SUPONE QUE PONIENDO NIEVE DA LO
+	 * MISMO QUE PONIENDO NUBES, ME PARECE RARO PERO ES LO QUE PONE EN LAS TABLAS
+	 * 
+	 */
 	@Test
 	public void esClimaCañasTRUE() throws Exception {
-	// Configuración del pronóstico
-		pronostico.setTemperatura(30); 
+		// Configuración del pronóstico
+		pronostico.setTemperatura(30);
 		pronostico.setPrecipitaciones(Precipitaciones.nieve);
 		espacio.setPronostico(pronostico);
-		
+
 		boolean actual = recomendador.esClimaCañas(pronostico);
 		boolean expected = true;
 		assertEquals(actual, expected);
-	
-		
+
 	}
 
 	/* test 12 (salida a true) */
 	@Test
 	public void esClimaPiscinaTRUE() throws Exception {
-	// Configuración del pronóstico
-		pronostico.setTemperatura(35); 
-		pronostico.setPrecipitaciones(Precipitaciones.nieve); 
+		// Configuración del pronóstico
+		pronostico.setTemperatura(35);
+		pronostico.setPrecipitaciones(Precipitaciones.nieve);
 		espacio.setPronostico(pronostico);
-		
+
 		boolean actual = recomendador.esClimaPlayaPiscina(pronostico);
 		boolean expected = true;
 		assertEquals(actual, expected);
-	
-		
+
 	}
 }
